160cd70572fb5d13d498063f6187bb8465d85424
solomon-eke <ekesolomon11@gmail.com>
2021-08-26 23:23:19 +0000

    I used -A to add all the files

---
+++ ice-cream.js
@@ -0,0 +1,16 @@
+// Do not modify these first two lines
+const iceCreamFlavours = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry",
+];
+// Add a Flavor
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
+
+// Your code below here...
